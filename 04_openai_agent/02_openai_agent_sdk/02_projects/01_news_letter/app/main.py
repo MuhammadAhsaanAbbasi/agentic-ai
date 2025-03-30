9479d173c6899c66644a2436101b678a65105634
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-03-31 02:53:05 +0500

    Add initial project files for news letter agent

---
+++ 04_openai_agent/02_openai_agent_sdk/02_projects/01_news_letter/app/main.py
@@ -0,0 +1,9 @@
+from agents import ( 
+    Agent, OpenAIChatCompletionsModel, AsyncOpenAI, Runner
+)
+
+# model = OpenAIChatCompletionsModel(
+#     model="deep",
+#     openai_client=AsyncOpenAI(base_url="")
+# )
+
